a6807005b3f0f6219969b1cf1bf1836183d41ec9
Rabbi Hossain Rifat <109733383+TheRabbiRifat@users.noreply.github.com>
2026-02-11 14:16:33 +0600

    Revert "Revert "Fix remaining dark ytheme issues in card headers""

--- resources/views/packages/create.blade.php
+++ resources/views/packages/create.blade.php
@@ -4,7 +4,7 @@
 <div class="row justify-content-center">
     <div class="col-md-8 col-lg-6">
         <div class="card shadow-sm border-0">
-            <div class="card-header bg-white py-3 border-0">
+            <div class="card-header py-3 border-0">
                 <h4 class="mb-0 fw-bold">Create New Package</h4>
             </div>
             <div class="card-body">
